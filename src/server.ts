30982ff30733c7a701d43baab527d2db0006f72d
Lucca Barros <luccabarros2003@gmail.com>
2024-10-02 17:22:12 -0300

    rota com listagem de filmes adicionada

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
+import { PrismaClient } from "@prisma/client";
 const port = 3000;
+const prisma = new PrismaClient();
 
 
-app.get('/movies', (req, res) => {
-    res.send('Listagem de filmes')
+app.get('/movies', async (req, res) => {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 })
 
 
