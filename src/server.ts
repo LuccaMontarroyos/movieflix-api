71ee4bcc55d8668163abf98225bc0f20706ea54c
Lucca Barros <luccabarros2003@gmail.com>
2024-10-06 16:25:09 -0300

    mudando a funcionalidade da rota de post da aplicação

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ const port = 3000;
 const prisma = new PrismaClient();
 
 
+app.use(express.json());
+
+
 app.get('/movies', async (req, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -16,6 +19,22 @@ app.get('/movies', async (req, res) => {
         }
     });
     res.json(movies);
+})
+
+app.post('/movies', async (req, res) => {
+   
+    const { title, genres_id, languages_id, release_date } = req.body;
+    
+    await prisma.movie.create({
+        data: {
+            title,
+            languages_id,
+            genres_id,
+            release_date: new Date(release_date)
+        }
+    })
+
+    res.status(201).send();
 })
 
 
