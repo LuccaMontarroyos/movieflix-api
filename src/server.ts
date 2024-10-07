2d475c1d6b03855f094171eb8998c79dd9eea670
Lucca Barros <luccabarros2003@gmail.com>
2024-10-06 21:40:46 -0300

    adicionando funcionalidade de atualizar dados de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import { PrismaClient } from "@prisma/client";
+import { equal } from "assert";
 const port = 3000;
 const prisma = new PrismaClient();
 
@@ -26,6 +27,23 @@ app.post('/movies', async (req, res) => {
     const { title, genres_id, languages_id, release_date } = req.body;
 
     try {
+        /*está sendo passado o título da requisição feita e através do método pego da tabela 'movie'
+        .findFirst, que lá ele tentará encontrar um movie onde o nome é igual ao titulo passado na requisição,
+        e usando o modo sensitive, que será armazenado na variável movieWithTheSameTitle, e assim fazemos a 
+        verificação*/
+        const movieWithTheSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: {
+                    equals: title,
+                    mode: 'insensitive'
+                }
+            }
+        });
+
+        if (movieWithTheSameTitle) {
+            return res.status(409).send({ message: 'Já existe um filme cadastrado com esse título' });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -39,6 +57,28 @@ app.post('/movies', async (req, res) => {
     }
 
     res.status(201).send();
+})
+
+/*Nesse caso como estamos fazendo a atualzação de um registro precisamos passar qual queremos
+alterar, portanto fazemos*/
+app.put('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+    const { genres_id, release_date } = req.body;
+    
+    try {
+        await prisma.movie.update({
+            where: {
+                id
+            },
+            data: {
+                genres_id,
+                release_date: new Date(release_date)
+            }
+        })
+        res.status(200).send();
+    } catch (error) {
+        res.status(500).send({ message: "Falha ao atualizar dados do filme"})
+    }
 })
 
 
